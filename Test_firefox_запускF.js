4d0ba187b7d63881f818549760445a50133190d7
IDirina <39695145+IDirina@users.noreply.github.com>
2018-06-14 00:05:18 +0300

    Add files via upload

---
+++ Test_firefox_запускF.js
@@ -0,0 +1,43 @@
+//Запуск в разных браузерах firefox
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+    .withCapabilities({'marionette': true})
+    .forBrowser('firefox')
+    .build();
+
+//задание 3 login
+
+var webdriver = require ('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
+
+var login = 'user2@localhost.ru';
+var password = 'qwerty';
+
+describe('Login', function() {
+    var driver;
+
+    before(function() {
+        driver = new webdriver.Builder()
+            .forBrowser('firefox')
+            .build();
+    });
+
+
+    it('should append query to title', function() {
+        driver.get('http://localhost/litecart/en/');
+
+        driver.findElement(By.css('[name=email]')).sendKeys(login);
+        driver.findElement(By.css('[name=password]')).sendKeys(password);
+        driver.findElement(By.css('[name=login]')).click();
+
+
+    });
+
+    after(function(){
+        //driver.quit();
+    });
+
+});
